cb18a5c4fbf54eb328c1ec2ca723922adb849c3a
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2024-08-27 19:43:51 +0600

    banner5 added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,12 +5,14 @@ import Banner3 from "../Banner3/Banner3";
 import FooterCard from "../FooterCard/FooterCard";
 import BannerTech from "../BannerTech/BannerTech";
 import BannerTalent from "../BannerTalent/BannerTalent";
+import BannerQuality from "../BannerQuality/BannerQuality";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Banner3></Banner3>
+      <BannerQuality></BannerQuality>
       <BannerTalent></BannerTalent>
       <BannerTech></BannerTech>
     </div>
