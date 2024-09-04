206c1db111b91a633fef0f976167428c7c8b3f86
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2024-09-04 14:07:31 +0600

    service page updated

--- src/pages/ServicePage/ServicePage/ServicePage.jsx
+++ src/pages/ServicePage/ServicePage/ServicePage.jsx
@@ -10,6 +10,7 @@ import mobile from "../../../assets/image/ServicePage/img3.png";
 import BannerCard2 from "../../../components/BannerCard2/BannerCard2";
 import background1 from "../../../assets/image/ServicePage/background1.png";
 import Button1 from "../../../components/buttons/Button1/Button1";
+import cardicon1 from "../../../assets/image/ServicePage/pngImg1.png";
 
 const ServicePage = () => {
   const [cards, setCards] = useState([]);
@@ -158,6 +159,150 @@ const ServicePage = () => {
             <Button1>
               <p className="px-8 text-lg">Book a Call</p>
             </Button1>
+          </div>
+        </div>
+
+        {/* 9th Section */}
+        <div>
+          <h2 className="text-xl text-center text-gray-300 py-5">
+            PROVEN EXPERTISE
+          </h2>
+          <h1 className="text-center lg:px-32 text-white text-5xl font-bold pb-20">
+            An extension of your team,{" "}
+            <span className="text-purple-400">
+              an accelerator for your ad design pipeline
+            </span>
+          </h1>
+          <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1  gap-4 px-4">
+            <div className="card   bg-gray-800 text-neutral-content w-96">
+              <div className="card-body items-center text-center">
+                <h2 className=" text-white">COST EFFECTIVE</h2>
+                <p className="text-7xl font-bold text-white">$250</p>
+                <p className="  text-white">saved on creative production</p>
+                <p className="  text-white">
+                  <span style={{ color: "#6dec93" }}>Imperfect Food</span>{" "}
+                  Stories{" "}
+                </p>
+              </div>
+            </div>
+
+            <div className="card   bg-gray-800 text-neutral-content w-96">
+              <div className="card-body items-center text-center">
+                <h2 className=" text-white">COST EFFECTIVE</h2>
+                <p className="text-7xl font-bold text-white">$250</p>
+                <p className="  text-white">saved on creative production</p>
+                <p className="  text-white">
+                  <span style={{ color: "#6dec93" }}>Imperfect Food</span>{" "}
+                  Stories{" "}
+                </p>
+              </div>
+            </div>
+            <div className="card   bg-gray-800 text-neutral-content w-96">
+              <div className="card-body items-center text-center">
+                <h2 className=" text-white">COST EFFECTIVE</h2>
+                <p className="text-7xl font-bold text-white">$250</p>
+                <p className="  text-white">saved on creative production</p>
+                <p className="  text-white">
+                  <span style={{ color: "#6dec93" }}>Imperfect Food</span>{" "}
+                  Stories{" "}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* 10th Section */}
+        <div>
+          <div className="hero  min-h-screen">
+            <div className="hero-content flex-col lg:flex-row">
+              <div className="lg:w-1/2 text-center lg:text-left">
+                <h2 className="text-gray-300  text-xl font-semibold">
+                  AD DESIGN FAQS
+                </h2>
+                <h1 className="text-5xl font-bold text-white">
+                  Answers to frequently asked questions
+                </h1>
+                <div
+                  className="bg-cover px-5  py-6 mt-7 rounded-md"
+                  style={{ backgroundImage: `url(${backImg})` }}
+                >
+                  <p className="py-6 text-white text-2xl font-semibold">
+                    Find the right solution for you now
+                  </p>
+                  <Button1>
+                    <p className="text-lg">Book a quick call</p>
+                  </Button1>
+                </div>
+              </div>
+              <div className="card   w-full max-w-sm shrink-0 shadow-2xl">
+                <div>
+                  <div className="collapse collapse-arrow ">
+                    <input type="radio" name="my-accordion-2" defaultChecked />
+                    <div className="collapse-title text-xl text-white font-medium">
+                      Click to open this one and close others
+                    </div>
+                    <div className="collapse-content text-gray-300">
+                      <p>hello</p>
+                    </div>
+                  </div>
+                  <div className="collapse collapse-arrow ">
+                    <input type="radio" name="my-accordion-2" />
+                    <div className="collapse-title text-xl text-white font-medium">
+                      Click to open this one and close others
+                    </div>
+                    <div className="collapse-content text-gray-300">
+                      <p>hello</p>
+                    </div>
+                  </div>
+                  <div className="collapse collapse-arrow ">
+                    <input type="radio" name="my-accordion-2" />
+                    <div className="collapse-title text-xl text-white font-medium">
+                      Click to open this one and close others
+                    </div>
+                    <div className="collapse-content text-gray-300">
+                      <p>hello</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* 11th Section */}
+        <div className="py-32">
+          <div className="grid lg:grid-cols-2  gap-7 px-4">
+            <div
+              style={{ backgroundImage: `url(${backImg})` }}
+              className="relative lg:row-span-2 bg-cover h-full lg:h-screen rounded-sm h-64 w-full px-5 py-6 bg-gray-900"
+            >
+              <img
+                className="lg:absolute hover:-translate-y-1 hover:scale-110 transition ease-in-out delay-150 lg:-top-20"
+                src={cardicon1}
+                alt=""
+              />
+              <h2 className="lg:mt-72 text-lg text-white ">PRICING</h2>
+              <h2 className="text-2xl pb-7 text-white font-bold ">
+                One Subscription for All of Your Creative Needs
+              </h2>
+              <div className="">
+                <Button1>
+                  <p className=" text-lg ">Get Started</p>
+                </Button1>
+              </div>
+            </div>
+            <div className=" h-64 rounded-sm w-full px-5 py-6 bg-gray-800">
+              <h2 className="text-lg text-white ">PRICING</h2>
+              <h2 className="text-2xl text-white font-bold ">
+                One Subscription for All of Your Creative Needs
+              </h2>
+            </div>
+            <div className=" h-64 rounded-sm w-full px-5 py-6 bg-gray-800">
+              <h2 className="text-lg text-white ">PRICING</h2>
+              <h2 className="text-2xl text-white font-bold ">
+                One Subscription for All of Your Creative Needs
+              </h2>
+            </div>
           </div>
         </div>
       </div>
